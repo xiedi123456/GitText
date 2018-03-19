b3aa0d5a4ca37f883b77d58cfd36114d3554f4fa
xiedi <441977321@qq.com>
2018-03-19 20:43:41 +0800

    增加方法Test()

--- src/main/java/Git.java
+++ src/main/java/Git.java
@@ -2,13 +2,13 @@
  * Created by john on 2018/3/18.
  */
 package main.java;
-
 public class Git {
     public static void main(String[] args) {
         System.out.println("dffd");
         test();
     }
     public static void test(){
-
+System.out.println("post");
+        System.out.println("GitText");
     }
         }
