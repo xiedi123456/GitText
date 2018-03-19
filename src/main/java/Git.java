e669676dd44c55e6e181cd0a3bbfe565ab5d1406
xiedi <441977321@qq.com>
2018-03-19 13:19:02 +0800

    第一次提交

--- src/main/java/Git.java
+++ src/main/java/Git.java
@@ -1,18 +1,21 @@
 /**
  * Created by john on 2018/3/18.
  */
+package main.java;
+
 public class Git {
     public static void main(String[] args) {
         System.out.println("dffd");
+        version();
     }
-    public void version()
+    public static void version()
     {
         System.out.println("感谢观看");
        int i=0;
         i=i+1;
         System.out.println("hello------");
         System.out.println("中文符号的修改");
-        System.out.println(“测试Fetch-------------------------------”）；
+
                                    System.out.println("dsadasdasdsad");
     }
         }
